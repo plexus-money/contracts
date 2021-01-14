c30251d20c7c79c90b33a36995b19f3409c78c9e
hellothere50 <hellothere50@hellothere50>
2021-01-15 00:34:55 +0900

    updated wrapper, for change and no loss checking/best paths

--- contracts/wrapper.sol
+++ contracts/wrapper.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: MIT
+//Previous deployment, almost done (unwrap is slightly different here for optimization): https://etherscan.io/address/0xdaca80c6b96815302b402ce4fe921ec654f364b1#readContract
+
 pragma solidity 0.7.4;
-//https://etherscan.io/address/0x1d17f9007282f9388bc9037688ade4344b2cc49b
 
 
 interface ERC20 {
@@ -119,6 +119,9 @@ contract WrapAndUnWrap{
   UniswapV2 uniswapExchange = UniswapV2(uniAddress);
   UniswapFactory factory = UniswapFactory(uniFactoryAddress);
   mapping (address => address[]) public lpTokenAddressToPairs;
+  mapping(string=>address) public stablecoins;
+  mapping(address=>mapping(address=>address[])) public presetPaths;
+  bool public changeRecpientIsOwner;
 
 
 
@@ -136,7 +139,10 @@ contract WrapAndUnWrap{
     }
 
   constructor() public payable {
-
+         stablecoins["DAI"] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+         stablecoins["USDT"] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+         stablecoins["USDC"] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+         changeRecpientIsOwner = false;
         owner= msg.sender;
 
   }
@@ -214,7 +220,7 @@ contract WrapAndUnWrap{
 
 
 
-        (,,uint liquidityCoins)  = uniswapExchange.addLiquidity(destinationTokens[0],destinationTokens[1], dTokenBalance, dTokenBalance2, 1,1, msg.sender, longTimeFromNow);
+        (,,uint liquidityCoins)  = uniswapExchange.addLiquidity(destinationTokens[0],destinationTokens[1], dTokenBalance, dTokenBalance2, 1,1, address(this), longTimeFromNow);
 
 
         address thisPairAddress = factory.getPair(destinationTokens[0],destinationTokens[1]);
@@ -222,6 +228,19 @@ contract WrapAndUnWrap{
         lpTokenAddressToPairs[thisPairAddress] =[destinationTokens[0], destinationTokens[1]];
         uint256 thisBalance =lpToken.balanceOf(address(this));
         lpToken.transfer(msg.sender, thisBalance);
+
+        //transfer any change to changeRecipient (from a pair imbalance. Should never be more than a few basis points)
+        address changeRecipient = msg.sender;
+        if(changeRecpientIsOwner == true){
+            changeRecipient = owner;
+        }
+        if(dToken.balanceOf(address(this)) >0){
+            dToken.transfer(changeRecipient, dToken.balanceOf(address(this)));
+        }
+        if(dToken2.balanceOf(address(this)) >0){
+            dToken2.transfer(changeRecipient, dToken2.balanceOf(address(this)));
+        }
+
         return (thisPairAddress,thisBalance) ;
       }
 
@@ -229,10 +248,32 @@ contract WrapAndUnWrap{
 
     }
 
+    function updateStableCoinAddress(string memory coinName, address newAddress) public onlyOwner returns(bool){
+        stablecoins[coinName] = newAddress;
+        return true;
+
+    }
+
+    function updatePresetPaths(address sellToken, address buyToken, address[] memory newPath ) public onlyOwner returns(bool){
+        presetPaths[sellToken][buyToken] = newPath;
+        return true;
+    }
+
+    //owner can turn on ability to collect a small fee from trade imbalances on LP conversions
+    function updateChangeRecipientBool(bool changeRecpientIsOwnerBool ) public onlyOwner returns(bool){
+        changeRecpientIsOwner = changeRecpientIsOwnerBool;
+        return true;
+    }
+
+
 
       function unwrap(address sourceToken, address destinationToken, uint256 amount) public payable returns( uint256){
 
+        address originalDestinationToken = destinationToken;
         ERC20 sToken = ERC20(sourceToken);
+        if(destinationToken == ETH_TOKEN_ADDRESS){
+            destinationToken = WETH_TOKEN_ADDRESS;
+        }
         ERC20 dToken = ERC20(destinationToken);
 
 
@@ -292,7 +333,7 @@ contract WrapAndUnWrap{
 
           if(lpTokenAddressToPairs[sourceToken].length !=0){
             sToken.approve(uniAddress, approvalAmount);
-          uniswapExchange.removeLiquidity(lpTokenAddressToPairs[sourceToken][0], lpTokenAddressToPairs[sourceToken][1], amount, 0,0, msg.sender, longTimeFromNow);
+          uniswapExchange.removeLiquidity(lpTokenAddressToPairs[sourceToken][0], lpTokenAddressToPairs[sourceToken][1], amount, 0,0, address(this), longTimeFromNow);
 
           ERC20 pToken1 = ERC20(lpTokenAddressToPairs[sourceToken][0]);
           ERC20 pToken2 = ERC20(lpTokenAddressToPairs[sourceToken][1]);
@@ -310,7 +351,16 @@ contract WrapAndUnWrap{
 
 
           uint256 destinationTokenBalance = dToken.balanceOf(address(this));
-          dToken.transfer(msg.sender, destinationTokenBalance);
+
+          if(originalDestinationToken == ETH_TOKEN_ADDRESS){
+              wethToken.withdraw(destinationTokenBalance);
+              msg.sender.transfer(address(this).balance);
+          }
+          else{
+               dToken.transfer(msg.sender, destinationTokenBalance);
+          }
+
+
           return destinationTokenBalance;
 
         }
@@ -372,9 +422,11 @@ contract WrapAndUnWrap{
                 wethToken.deposit{value:msg.value}();
             }
             else if(sellToken == address(0x0)){
-                address [] memory addresses = new address[](2);
-                addresses[0] = WETH_TOKEN_ADDRESS;
-                addresses[1] = buyToken;
+
+               // address [] memory addresses = new address[](2);
+               address [] memory addresses = getBestPath(WETH_TOKEN_ADDRESS, buyToken, amount);
+                //addresses[0] = WETH_TOKEN_ADDRESS;
+                //addresses[1] = buyToken;
                 uniswapExchange.swapExactETHForTokens{value:msg.value}(0, addresses, address(this), 1000000000000000 );
 
             }
@@ -382,9 +434,10 @@ contract WrapAndUnWrap{
             else if(sellToken == WETH_TOKEN_ADDRESS){
                 wethToken.withdraw(amount);
 
-                address [] memory addresses = new address[](2);
-                addresses[0] = WETH_TOKEN_ADDRESS;
-                addresses[1] = buyToken;
+                //address [] memory addresses = new address[](2);
+                address [] memory addresses = getBestPath(WETH_TOKEN_ADDRESS, buyToken, amount);
+                //addresses[0] = WETH_TOKEN_ADDRESS;
+                //addresses[1] = buyToken;
                 uniswapExchange.swapExactETHForTokens{value:amount}(0, addresses, address(this), 1000000000000000 );
 
             }
@@ -392,13 +445,121 @@ contract WrapAndUnWrap{
 
 
             else{
-          address [] memory addresses = new address[](2);
-          addresses[0] = sellToken;
-          addresses[1] = buyToken;
+          //address [] memory addresses = new address[](2);
+         // addresses[0] = sellToken;
+          //addresses[1] = buyToken;
+          address [] memory addresses = getBestPath(sellToken, buyToken, amount);
            uint256 [] memory amounts = conductUniswapT4T(addresses, amount );
-           uint256 resultingTokens = amounts[1];
+           uint256 resultingTokens = amounts[amounts.length-1];
            return resultingTokens;
             }
+    }
+
+
+    //gets the best path to route the transaction on Uniswap
+    function getBestPath(address sellToken, address buyToken, uint256 amount) public view returns (address[] memory){
+
+        address [] memory defaultPath =new address[](2);
+        defaultPath[0]=sellToken;
+        defaultPath[1] = buyToken;
+
+
+        if(presetPaths[sellToken][buyToken].length !=0){
+            return presetPaths[sellToken][buyToken];
+        }
+
+
+        if(sellToken == stablecoins["DAI"] || sellToken == stablecoins["USDC"] || sellToken == stablecoins["USDT"]){
+            return defaultPath;
+        }
+        if(buyToken == stablecoins["DAI"] || buyToken == stablecoins["USDC"] || buyToken == stablecoins["USDT"]){
+            return defaultPath;
+        }
+
+
+
+        address[] memory daiPath = new address[](3);
+        address[] memory usdcPath =new address[](3);
+        address[] memory usdtPath =new address[](3);
+
+        daiPath[0] = sellToken;
+        daiPath[1] = stablecoins["DAI"];
+        daiPath[2] = buyToken;
+
+        usdcPath[0] = sellToken;
+        usdcPath[1] = stablecoins["USDC"];
+        usdcPath[2] = buyToken;
+
+        usdtPath[0] = sellToken;
+        usdtPath[1] = stablecoins["USDT"];
+        usdtPath[2] = buyToken;
+
+
+        uint256 directPathOutput =  getPriceFromUniswap(defaultPath, amount)[1];
+
+
+        uint256[] memory daiPathOutputRaw = getPriceFromUniswap(daiPath, amount);
+        uint256[]  memory usdtPathOutputRaw = getPriceFromUniswap(usdtPath, amount);
+        uint256[]  memory usdcPathOutputRaw = getPriceFromUniswap(usdcPath, amount);
+
+        //uint256 directPathOutput = directPathOutputRaw[directPathOutputRaw.length-1];
+        uint256 daiPathOutput = daiPathOutputRaw[daiPathOutputRaw.length-1];
+        uint256 usdtPathOutput = usdtPathOutputRaw[usdtPathOutputRaw.length-1];
+        uint256 usdcPathOutput = usdcPathOutputRaw[usdcPathOutputRaw.length-1];
+
+        uint256 bestPathOutput = directPathOutput;
+        address[] memory bestPath = new address[](2);
+        address[] memory bestPath3 = new address[](3);
+        //return defaultPath;
+        bestPath = defaultPath;
+
+        bool isTwoPath = true;
+
+        if(directPathOutput < daiPathOutput){
+            isTwoPath=false;
+            bestPathOutput = daiPathOutput;
+            bestPath3 = daiPath;
+        }
+        if(bestPathOutput < usdcPathOutput){
+            isTwoPath=false;
+            bestPathOutput = usdcPathOutput;
+            bestPath3 = usdcPath;
+        }
+         if(bestPathOutput < usdtPathOutput){
+             isTwoPath=false;
+            bestPathOutput = usdtPathOutput;
+            bestPath3 = usdtPath;
+        }
+
+        require(bestPathOutput >0, "This trade will result in getting zero tokens back. Reverting");
+
+        if(isTwoPath==true){
+              return bestPath;
+        }
+        else{
+            return bestPath3;
+        }
+
+
+
+    }
+
+    function getPriceFromUniswap(address  [] memory theAddresses, uint amount) public view returns (uint256[] memory amounts1){
+
+
+        try uniswapExchange.getAmountsOut(amount,theAddresses ) returns (uint256[] memory amounts){
+            return amounts;
+        }
+        catch  {
+            uint256 [] memory amounts2= new uint256[](2);
+            amounts2[0]=0;
+            amounts2[1]=0;
+            return amounts2;
+
+        }
+
+        //return amounts;
+
     }
 
     function conductUniswapT4T(address  [] memory theAddresses, uint amount) internal returns (uint256[] memory amounts1){
