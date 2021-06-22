48428693b2dd7625f3e945a370af851d5d69af7e
Lucifer H <trmlucifer2020@gmail.com>
2021-06-22 06:27:23 -0700

    contracts cleanup and deployment script

--- contracts/tier1Staking.sol
+++ contracts/tier1Staking.sol
@@ -1,75 +1,15 @@
 // SPDX-License-Identifier: MIT
-//Mainnet: 0x97b00db19bAe93389ba652845150CAdc597C6B2F
-pragma solidity >=0.4.22 <0.8.0;
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./proxyLib/OwnableUpgradeable.sol";
+import "./interfaces/IPlexusOracle.sol";
+import "./interfaces/staking/ITokenRewards.sol";
+import "./interfaces/staking/ITier2Staking.sol";
 
-interface ERC20 {
-    function totalSupply() external view returns(uint supply);
-    function balanceOf(address _owner) external view returns(uint balance);
-    function transfer(address _to, uint _value) external returns(bool success);
-    function transferFrom(address _from, address _to, uint _value) external returns(bool success);
-    function approve(address _spender, uint _value) external returns(bool success);
-    function allowance(address _owner, address _spender) external view returns(uint remaining);
-    function decimals() external view returns(uint digits);
-    event Approval(address indexed _owner, address indexed _spender, uint _value);
-}
-
-interface Tier2StakingInterface {
-    //staked balance info
-    function depositBalances(address _owner, address token) external view returns(uint256 balance);
-    function getStakedBalances(address _owner, address token) external view returns(uint256 balance);
-    function getStakedPoolBalanceByUser(address _owner, address tokenAddress) external view returns(uint256);
-    //basic info
-    function tokenToFarmMapping(address tokenAddress) external view returns(address stakingContractAddress);
-    function stakingContracts(string calldata platformName) external view returns(address stakingAddress);
-    function stakingContractsStakingToken(string calldata platformName) external view returns(address tokenAddress);
-    function platformToken() external view returns(address tokenAddress);
-    function owner() external view returns(address ownerAddress);
-    //actions
-    function deposit(address tokenAddress, uint256 amount, address onBehalfOf) payable external returns (bool);
-    function withdraw(address tokenAddress, uint256 amount, address payable onBehalfOf) payable external returns(bool);
-    function addOrEditStakingContract(string calldata name, address stakingAddress, address stakingToken ) external  returns (bool);
-    function updateCommission(uint amount) external  returns(bool);
-    function changeOwner(address payable newOwner) external returns (bool);
-    function adminEmergencyWithdrawTokens(address token, uint amount, address payable destination) external returns(bool);
-    function kill() virtual external;
-}
-
-interface Oracle {
-    function getAddress(string memory) view external returns (address);
-}
-
-interface Rewards {
-  function unstakeAndClaimDelegated(address onBehalfOf, address tokenAddress, address recipient) external returns (uint256);
-  function stakeDelegated(uint256 amount, address tokenAddress, address onBehalfOf) external returns(bool);
-  function checkIfTokenIsWhitelistedForStaking(address tokenAddress) external view returns(bool);
-}
-
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal view returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal view returns (uint256) {
-        assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal view returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal view returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
+// Tier1FarmController contract on Mainnet: 0x97b00db19bAe93389ba652845150CAdc597C6B2F
 
 contract Tier1FarmController is OwnableUpgradeable {
     using SafeMath
@@ -78,20 +18,20 @@ contract Tier1FarmController is OwnableUpgradeable {
     address ETH_TOKEN_ADDRESS;
     mapping (string => address) public tier2StakingContracts;
     uint256 public commission; // Default is 4 percent
-    Oracle oracle;
+    IPlexusOracle private oracle;
     address public oracleAddress;
     string public farmName;
-    mapping (address => uint256) totalAmountStaked;
+    mapping(address => uint256) private totalAmountStaked;
 
-    constructor() public payable {
+    constructor() payable {
     }
 
-    function initialize() initializeOnceOnly public {
+    function initialize(address _tier2StakingContracts_farm, address _oracleAddress) initializeOnceOnly public {
         ETH_TOKEN_ADDRESS  = address(0x0);
         commission  = 400; // Default is 4 percent
         farmName = 'Tier1Aggregator';
-        tier2StakingContracts["FARM"] = 0x618fDCFF3Cca243c12E6b508D9d8a6fF9018325c;
-        updateOracleAddress(0xBDfF00110c97D0FE7Fefbb78CE254B12B9A7f41f);
+        tier2StakingContracts["FARM"] = _tier2StakingContracts_farm;
+        updateOracleAddress(_oracleAddress);
     }
 
     modifier onlyAdmin {
@@ -105,107 +45,152 @@ contract Tier1FarmController is OwnableUpgradeable {
     fallback() external payable {
     }
 
-    function updateOracleAddress(address newOracleAddress ) public onlyOwner returns (bool) {
-        oracleAddress= newOracleAddress;
-        oracle = Oracle(newOracleAddress);
+    receive() external payable {
+    }
+
+    function updateOracleAddress(address newOracleAddress) public onlyOwner returns (bool) {
+        oracleAddress = newOracleAddress;
+        oracle = IPlexusOracle(newOracleAddress);
         return true;
     }
 
-    function addOrEditTier2ChildStakingContract(string memory name, address stakingAddress ) public onlyOwner returns (bool) {
+    function addOrEditTier2ChildStakingContract(
+        string memory name,
+        address stakingAddress
+    ) public onlyOwner returns (bool) {
         tier2StakingContracts[name] = stakingAddress;
         return true;
     }
 
-    function addOrEditTier2ChildsChildStakingContract(address tier2Contract, string memory name, address stakingAddress, address stakingToken ) public onlyOwner returns (bool) {
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
+    function addOrEditTier2ChildsChildStakingContract(
+        address tier2Contract,
+        string memory name,
+        address stakingAddress,
+        address stakingToken
+    ) public onlyOwner returns (bool) {
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
         tier2Con.addOrEditStakingContract(name, stakingAddress, stakingToken);
         return true;
     }
 
-    function updateCommissionTier2(address tier2Contract, uint amount) public onlyOwner returns(bool) {
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
+    function updateCommissionTier2(address tier2Contract, uint256 amount) public onlyOwner returns (bool) {
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
         tier2Con.updateCommission(amount);
         return true;
     }
 
-    function deposit(string memory tier2ContractName, address tokenAddress, uint256 amount, address payable onBehalfOf) onlyAdmin payable public returns (bool) {
+    function deposit(
+        string memory tier2ContractName,
+        address tokenAddress,
+        uint256 amount,
+        address payable onBehalfOf
+    ) public payable onlyAdmin returns (bool) {
         address tier2Contract = tier2StakingContracts[tier2ContractName];
-        ERC20 thisToken = ERC20(tokenAddress);
-        require(thisToken.transferFrom(msg.sender, address(this), amount), "Not enough tokens to transferFrom or no approval");
-        //approve the tier2 contract to handle tokens from this account
+        IERC20 thisToken = IERC20(tokenAddress);
+        require(
+            thisToken.transferFrom(msg.sender, address(this), amount),
+            "Not enough tokens to transferFrom or no approval"
+        );
+
+        // approve the tier2 contract to handle tokens from this account
         thisToken.approve(tier2Contract, 0);
         thisToken.approve(tier2Contract, amount.mul(100));
 
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
+
         tier2Con.deposit(tokenAddress, amount, onBehalfOf);
+
         address rewardsContract = oracle.getAddress("REWARDS");
 
         if (rewardsContract != address(0x0)) {
-            Rewards rewards = Rewards(rewardsContract);
+            ITokenRewards rewards = ITokenRewards(rewardsContract);
             if (rewards.checkIfTokenIsWhitelistedForStaking(tokenAddress)) {
-              rewards.stakeDelegated(amount, tokenAddress, onBehalfOf);
+                rewards.stakeDelegated(amount, tokenAddress, onBehalfOf);
             }
         }
-
         return true;
     }
 
-    function withdraw(string memory tier2ContractName, address tokenAddress, uint256 amount, address payable onBehalfOf) onlyAdmin payable public returns(bool) {
+    function withdraw(
+        string memory tier2ContractName,
+        address tokenAddress,
+        uint256 amount,
+        address payable onBehalfOf
+    ) public payable onlyAdmin returns (bool) {
         address tier2Contract = tier2StakingContracts[tier2ContractName];
-        ERC20 thisToken = ERC20(tokenAddress);
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
         tier2Con.withdraw(tokenAddress, amount, onBehalfOf);
         address rewardsContract = oracle.getAddress("REWARDS");
-
         if (rewardsContract != address(0x0)) {
-          Rewards rewards = Rewards(rewardsContract);
-          if (rewards.checkIfTokenIsWhitelistedForStaking(tokenAddress)) {
-            rewards.unstakeAndClaimDelegated(onBehalfOf, tokenAddress, onBehalfOf);
-          }
+            ITokenRewards rewards = ITokenRewards(rewardsContract);
+            if (rewards.checkIfTokenIsWhitelistedForStaking(tokenAddress)) {
+                rewards.unstakeAndClaimDelegated(
+                    onBehalfOf,
+                    tokenAddress,
+                    onBehalfOf
+                );
+            }
         }
-
         return true;
     }
 
-
-    function changeTier2Owner(address payable tier2Contract, address payable newOwner) onlyOwner public returns (bool) {
-         Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
-         tier2Con.changeOwner(newOwner);
-         return true;
+    function changeTier2Owner(
+        address payable tier2Contract,
+        address payable newOwner
+    ) public onlyOwner returns (bool) {
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
+        tier2Con.changeOwner(newOwner);
+        return true;
     }
 
-    function adminEmergencyWithdrawTokensTier2(address payable tier2Contract, address token, uint amount, address payable destination) public onlyOwner returns(bool) {
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
+    function adminEmergencyWithdrawTokensTier2(
+        address payable tier2Contract,
+        address token,
+        uint256 amount,
+        address payable destination
+    ) public onlyOwner returns (bool) {
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
         tier2Con.adminEmergencyWithdrawTokens(token, amount, destination);
         return true;
     }
 
-    function adminEmergencyWithdrawTokens(address token, uint amount, address payable destination) public onlyOwner returns(bool) {
-      if (address(token) == ETH_TOKEN_ADDRESS) {
-          destination.transfer(amount);
-      } else {
-          ERC20 tokenToken = ERC20(token);
-          require(tokenToken.transfer(destination, amount));
-      }
+    function adminEmergencyWithdrawTokens(
+        address token,
+        uint256 amount,
+        address payable destination
+    ) public onlyOwner returns (bool) {
+        if (address(token) == ETH_TOKEN_ADDRESS) {
+            destination.transfer(amount);
+        } else {
+            IERC20 token_ = IERC20(token);
+            require(
+                token_.transfer(destination, amount),
+                "Token transfer failed"
+            );
+        }
 
-      return true;
+        return true;
     }
 
-    function getStakedPoolBalanceByUser(string memory tier2ContractName, address _owner, address tokenAddress) public view returns(uint256) {
+    function getStakedPoolBalanceByUser(
+        string memory tier2ContractName,
+        address _owner,
+        address tokenAddress
+    ) public view returns (uint256) {
         address tier2Contract = tier2StakingContracts[tier2ContractName];
-        ERC20 thisToken = ERC20(tokenAddress);
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
-        uint balance = tier2Con.getStakedPoolBalanceByUser(_owner, tokenAddress);
-
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
+        uint256 balance = tier2Con.getStakedPoolBalanceByUser(_owner, tokenAddress);
         return balance;
     }
 
-    function getDepositBalanceByUser(string calldata tier2ContractName, address _owner, address token) external view returns(uint256 ) {
+    function getDepositBalanceByUser(
+        string calldata tier2ContractName,
+        address _owner,
+        address token
+    ) external view returns (uint256) {
         address tier2Contract = tier2StakingContracts[tier2ContractName];
-        ERC20 thisToken = ERC20(token);
-        Tier2StakingInterface tier2Con = Tier2StakingInterface(tier2Contract);
-        uint balance = tier2Con.depositBalances(_owner, token);
-
+        ITier2Staking tier2Con = ITier2Staking(tier2Contract);
+        uint256 balance = tier2Con.depositBalances(_owner, token);
         return balance;
     }
 }
