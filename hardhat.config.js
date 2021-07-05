14b137edfc724c17b6761dbde5eff081edcb9722
Lucifer H <trmlucifer2020@gmail.com>
2021-07-06 07:17:52 +0800

    change the deployment script for bsc/matic

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,60 +1,86 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("dotenv").config();
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-solhint');
-require('solidity-coverage');
-
-module.exports = {
-  defaultNetwork: "hardhat",
-  solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  networks: {
-    hardhat: {
-      gas: "auto",
-      gasPrice: "auto",
-      gasMultiplier: 20,
-      blockGasLimit: 90000000000000,
-      forking: {
-        url: process.env.RPC_NODE_URL,
-        blockNumber: 11997864,
-      }
-    },
-
-    mainnet: {
-      gas: "auto",
-      gasPrice: "auto",
-      gasMultiplier: 20,
-      blockGasLimit: 90000000000000,
-      accounts: {
-        mnemonic: process.env.MNEMONIC
-      },
-      url: process.env.RPC_NODE_URL,
-      chainId: 1
-    },
-    
-    kovan: {
-      gas: "auto",
-      gasPrice: "auto",
-      gasMultiplier: 20,
-      blockGasLimit: 90000000000000,
-      accounts: {
-        mnemonic: process.env.MNEMONIC
-      },
-      url: process.env.RPC_NODE_URL_KOVAN,
-      chainId: 42
-    }
-  },
-
-  mocha: {
-    timeout: 2000000
-  }
-};
+ require("dotenv").config();
+ require('@nomiclabs/hardhat-waffle');
+ require('@nomiclabs/hardhat-solhint');
+ require('solidity-coverage');
+ 
+ module.exports = {
+   defaultNetwork: "hardhat",
+   solidity: {
+     version: "0.8.0",
+     settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+     }
+   },
+   networks: {
+     hardhat: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       forking: {
+         url: process.env.RPC_NODE_URL,
+         blockNumber: 11997864,
+       }
+     },
+ 
+     mainnet: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL,
+       chainId: 1
+     },
+     
+     kovan: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL_KOVAN,
+       chainId: 42
+     },
+     
+     binance: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL_BINANCE,
+       chainId: 56
+     },
+     
+     matic: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL_MATIC,
+       chainId: 137
+     },
+ 
+   },
+ 
+   mocha: {
+     timeout: 2000000
+   }
+ };
+ 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,11 @@ async function main() {
 	const addr = config.addresses;
 
 	const netinfo = await ethers.provider.getNetwork();
-	var network = netinfo.name;
+	var network = netinfo.chainId === 1 ? "mainnet" :
+					netinfo.chainId === 42 ? "kovan" : 
+					netinfo.chainId === 56 ? "binance" :
+					netinfo.chainId === 137 ? "matic" :
+					"mainnet";
 	if (network === "unknown")
 		network = "mainnet";
 
@@ -29,6 +33,7 @@ async function main() {
 	const Tier2Pickle = await ethers.getContractFactory('Tier2PickleFarmController');
 	const LP2LP = await ethers.getContractFactory('LP2LP');
 	const Tier2Aggregator = await ethers.getContractFactory('Tier2AggregatorFarmController');
+	const Airdrop = await ethers.getContractFactory('Airdrop');
 
 	// plexus reward token
 	const PlexusCoin = await ethers.getContractFactory('PlexusTestCoin');
@@ -43,143 +48,431 @@ async function main() {
 	console.log("");
 
 	// then deploy the contracts and wait for them to be mined
-	const wrapper = await deployWithProxy(
-        Wrapper, 
-        OwnableProxy, 
-        'WrapAndUnWrap',
-		addr.tokens.WETH[network],
-		addr.swaps.uniswap[network],
-		addr.swaps.uniswapFactory[network],
-		addr.tokens.DAI[network],
-		addr.tokens.USDT[network],
-		addr.tokens.USDC[network]
-    );
-	console.log("Wrapper is deployed at: ", wrapper.address);
+	if (network === 'mainnet') {
+		const wrapper = await deployWithProxy(
+			Wrapper, 
+			OwnableProxy, 
+			'WrapAndUnWrap',
+			addr.tokens.WETH[network],
+			addr.swaps.uniswap[network],
+			addr.swaps.uniswapFactory[network],
+			addr.tokens.DAI[network],
+			addr.tokens.USDT[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("Wrapper is deployed at: ", wrapper.address);
 
-	const wrapperSushi = await deployWithProxy(
-        WrapperSushi, 
-        OwnableProxy, 
-        'WrapAndUnWrapSushi',
-		addr.tokens.WETH[network],
-		addr.swaps.sushiswap[network],
-		addr.swaps.uniswapFactory[network],
-		addr.tokens.DAI[network],
-		addr.tokens.USDT[network],
-		addr.tokens.USDC[network]
-    );
-	console.log("WrapperSushi is deployed at: ", wrapperSushi.address);
+		const wrapperSushi = await deployWithProxy(
+			WrapperSushi, 
+			OwnableProxy, 
+			'WrapAndUnWrapSushi',
+			addr.tokens.WETH[network],
+			addr.swaps.sushiswap[network],
+			addr.swaps.uniswapFactory[network],
+			addr.tokens.DAI[network],
+			addr.tokens.USDT[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("WrapperSushi is deployed at: ", wrapperSushi.address);
 
-	const tokenRewards = await deployWithProxy(
-        TokenRewards,
-        OwnableProxy,
-        'TokenRewards'
-    );
-	console.log("TokenRewards is deployed at: ", tokenRewards.address);
+		const tokenRewards = await deployWithProxy(
+			TokenRewards,
+			OwnableProxy,
+			'TokenRewards'
+		);
+		console.log("TokenRewards is deployed at: ", tokenRewards.address);
+	
+		const plexusOracle = await deployWithProxy(
+			PlexusOracle, 
+			OwnableProxy, 
+			'PlexusOracle',
+			addr.swaps.uniswap[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("PlexusOracle is deployed at: ", plexusOracle.address);
+	
+		const tier2Farm = await deployWithProxy(
+			Tier2Farm, 
+			OwnableProxy, 
+			'Tier2FarmController',
+			addr.AutoStake[network],
+			addr.tokens.FARM[network]
+		);
+		console.log("Tier2Farm is deployed at: ", tier2Farm.address);
+	
+		const tier1Staking = await deployWithProxy(
+			Tier1Staking, 
+			OwnableProxy, 
+			'Tier1FarmController',
+			tier2Farm.address,
+			plexusOracle.address
+		);
+		console.log("Tier1Staking is deployed at: ", tier1Staking.address);
+	
+		const core = await deployWithProxy(
+			Core, 
+			OwnableProxy, 
+			'Core',
+			addr.tokens.WETH[network],
+			wrapper.address
+		);
+		console.log("Core is deployed at: ", core.address);
+	
+		const tier2Aave = await deployWithProxy(
+			Tier2Aave, 
+			OwnableProxy, 
+			'Tier2AaveFarmController',
+			addr.AaveLendingPoolV2[network],
+			addr.tokens.DAI[network],
+			addr.tokens.aDAI[network]
+		);
+		console.log("Tier2Aave is deployed at: ", tier2Aave.address);
+	
+		const tier2Pickle = await deployWithProxy(
+			Tier2Pickle, 
+			OwnableProxy, 
+			'Tier2PickleFarmController',
+			addr.StakingRewards[network],
+			addr.tokens.PICKLE[network]
+		);
+		console.log("Tier2Pickle is deployed at: ", tier2Pickle.address);
+	
+		const lp2lp = await deployWithProxy(
+			LP2LP, 
+			OwnableProxy, 
+			'LP2LP',
+			addr.tokens.vBNT[network]
+		);
+		console.log("LP2LP is deployed at: ", lp2lp.address);
+	
+		const tier2Aggregator = await deployWithProxy(
+			Tier2Aggregator, 
+			OwnableProxy, 
+			'Tier2AggregatorFarmController',
+			addr.tokens.pUNI_V2[network],
+			addr.tokens.UNI_V2[network]
+		);
+		console.log("Tier2Aggregator is deployed at: ", tier2Aggregator.address);
+	
+		const plexusCoin = await (await PlexusCoin.deploy()).deployed();
+		console.log("PlexusCoin is deployed at: ", plexusCoin.address);
 
-	const plexusOracle = await deployWithProxy(
-        PlexusOracle, 
-        OwnableProxy, 
-        'PlexusOracle',
-		addr.swaps.uniswap[network],
-		addr.tokens.USDC[network]
-    );
-	console.log("PlexusOracle is deployed at: ", plexusOracle.address);
+		// airdrop
+		const airdrop = await deployWithProxy(
+			Airdrop, 
+			OwnableProxy, 
+			'Airdrop',
+			plexusCoin.address,
+			[addr1.address, addrs[0].address]
+		);
+		console.log("Airdrop is deployed at: ", airdrop.address);
+	
+		console.log("");
+		// then setup the contracts
+		console.log("Setting up contracts... ");
+		await tokenRewards.updateOracleAddress(plexusOracle.address);
+		await tokenRewards.updateStakingTokenAddress(plexusCoin.address);
+	 
+		await plexusOracle.updateRewardAddress(tokenRewards.address);
+		await plexusOracle.updateCoreAddress(core.address);
+		await plexusOracle.updateTier1Address(tier1Staking.address);
+	
+		await core.setOracleAddress(plexusOracle.address);
+		await core.setStakingAddress(tier1Staking.address);
+		await core.setConverterAddress(wrapper.address);
+	
+		await tier1Staking.updateOracleAddress(plexusOracle.address);
+	
+		console.log("");
+		// setup tier 1 staking
+		console.log("Setting up Tier1Staking... ");
+		await tier1Staking.addOrEditTier2ChildStakingContract("FARM", tier2Farm.address);
+		await tier1Staking.addOrEditTier2ChildStakingContract("DAI", tier2Aave.address);
+		await tier1Staking.addOrEditTier2ChildStakingContract("PICKLE", tier2Pickle.address);
+	
+		console.log("");
+		// setup ownership of tier2 contracts ownership
+		console.log("Setting up ownerships of Tier2 contracts... ");
+		await tier2Farm.changeOwner(tier1Staking.address);
+		await tier2Aave.changeOwner(tier1Staking.address);
+		await tier2Pickle.changeOwner(tier1Staking.address);
+	} else if (network === 'binance') {
+		const wrapper = await deployWithProxy(
+			Wrapper, 
+			OwnableProxy, 
+			'WrapAndUnWrap',
+			addr.tokens.WETH[network],
+			addr.swaps.pancakeswap[network],
+			addr.swaps.pancakeswapFactory[network],
+			addr.tokens.DAI[network],
+			addr.tokens.USDT[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("Wrapper is deployed at: ", wrapper.address);
 
-	const tier2Farm = await deployWithProxy(
-        Tier2Farm, 
-        OwnableProxy, 
-        'Tier2FarmController',
-		addr.AutoStake[network],
-		addr.tokens.FARM[network]
-    );
-	console.log("Tier2Farm is deployed at: ", tier2Farm.address);
+		const tokenRewards = await deployWithProxy(
+			TokenRewards,
+			OwnableProxy,
+			'TokenRewards'
+		);
+		console.log("TokenRewards is deployed at: ", tokenRewards.address);
+	
+		const plexusOracle = await deployWithProxy(
+			PlexusOracle, 
+			OwnableProxy, 
+			'PlexusOracle',
+			addr.swaps.pancakeswap[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("PlexusOracle is deployed at: ", plexusOracle.address);
+	
+		const core = await deployWithProxy(
+			Core, 
+			OwnableProxy, 
+			'Core',
+			addr.tokens.WETH[network],
+			wrapper.address
+		);
+		console.log("Core is deployed at: ", core.address);
+	
+		const plexusCoin = await (await PlexusCoin.deploy()).deployed();
+		console.log("PlexusCoin is deployed at: ", plexusCoin.address);
 
-	const tier1Staking = await deployWithProxy(
-        Tier1Staking, 
-        OwnableProxy, 
-        'Tier1FarmController',
-		tier2Farm.address,
-		plexusOracle.address
-    );
-	console.log("Tier1Staking is deployed at: ", tier1Staking.address);
+		// airdrop
+		const airdrop = await deployWithProxy(
+			Airdrop, 
+			OwnableProxy, 
+			'Airdrop',
+			plexusCoin.address,
+			[addr1.address, addrs[0].address]
+		);
+		console.log("Airdrop is deployed at: ", airdrop.address);
+	
+		console.log("");
+		// then setup the contracts
+		console.log("Setting up contracts... ");
+		await tokenRewards.updateOracleAddress(plexusOracle.address);
+		await tokenRewards.updateStakingTokenAddress(plexusCoin.address);
+	 
+		await plexusOracle.updateRewardAddress(tokenRewards.address);
+		await plexusOracle.updateCoreAddress(core.address);
+		await plexusOracle.updateTier1Address(tier1Staking.address);
+	
+		await core.setOracleAddress(plexusOracle.address);
+		await core.setConverterAddress(wrapper.address);
+	} else if (network === 'matic') {
+		const wrapper = await deployWithProxy(
+			Wrapper, 
+			OwnableProxy, 
+			'WrapAndUnWrap',
+			addr.tokens.WETH[network],
+			addr.swaps.quickswap[network],
+			addr.swaps.quickswapFactory[network],
+			addr.tokens.DAI[network],
+			addr.tokens.USDT[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("Wrapper is deployed at: ", wrapper.address);
 
-	const core = await deployWithProxy(
-        Core, 
-        OwnableProxy, 
-        'Core',
-		addr.tokens.WETH[network],
-		wrapper.address
-    );
-	console.log("Core is deployed at: ", core.address);
+		const tokenRewards = await deployWithProxy(
+			TokenRewards,
+			OwnableProxy,
+			'TokenRewards'
+		);
+		console.log("TokenRewards is deployed at: ", tokenRewards.address);
+	
+		const plexusOracle = await deployWithProxy(
+			PlexusOracle, 
+			OwnableProxy, 
+			'PlexusOracle',
+			addr.swaps.quickswap[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("PlexusOracle is deployed at: ", plexusOracle.address);
+	
+		const core = await deployWithProxy(
+			Core, 
+			OwnableProxy, 
+			'Core',
+			addr.tokens.WETH[network],
+			wrapper.address
+		);
+		console.log("Core is deployed at: ", core.address);
+	
+		const plexusCoin = await (await PlexusCoin.deploy()).deployed();
+		console.log("PlexusCoin is deployed at: ", plexusCoin.address);
 
-	const tier2Aave = await deployWithProxy(
-        Tier2Aave, 
-        OwnableProxy, 
-        'Tier2AaveFarmController',
-		addr.AaveLendingPoolV2[network],
-		addr.tokens.DAI[network],
-		addr.tokens.aDAI[network]
-    );
-	console.log("Tier2Aave is deployed at: ", tier2Aave.address);
+		// airdrop
+		const airdrop = await deployWithProxy(
+			Airdrop, 
+			OwnableProxy, 
+			'Airdrop',
+			plexusCoin.address,
+			[addr1.address, addrs[0].address]
+		);
+		console.log("Airdrop is deployed at: ", airdrop.address);
+	
+		console.log("");
+		// then setup the contracts
+		console.log("Setting up contracts... ");
+		await tokenRewards.updateOracleAddress(plexusOracle.address);
+		await tokenRewards.updateStakingTokenAddress(plexusCoin.address);
+	 
+		await plexusOracle.updateRewardAddress(tokenRewards.address);
+		await plexusOracle.updateCoreAddress(core.address);
+		await plexusOracle.updateTier1Address(tier1Staking.address);
+	
+		await core.setOracleAddress(plexusOracle.address);
+		await core.setConverterAddress(wrapper.address);
+	} else {
+		const wrapper = await deployWithProxy(
+			Wrapper, 
+			OwnableProxy, 
+			'WrapAndUnWrap',
+			addr.tokens.WETH[network],
+			addr.swaps.uniswap[network],
+			addr.swaps.uniswapFactory[network],
+			addr.tokens.DAI[network],
+			addr.tokens.USDT[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("Wrapper is deployed at: ", wrapper.address);
 
-	const tier2Pickle = await deployWithProxy(
-        Tier2Pickle, 
-        OwnableProxy, 
-        'Tier2PickleFarmController',
-		addr.StakingRewards[network],
-		addr.tokens.PICKLE[network]
-    );
-	console.log("Tier2Pickle is deployed at: ", tier2Pickle.address);
+		const wrapperSushi = await deployWithProxy(
+			WrapperSushi, 
+			OwnableProxy, 
+			'WrapAndUnWrapSushi',
+			addr.tokens.WETH[network],
+			addr.swaps.sushiswap[network],
+			addr.swaps.uniswapFactory[network],
+			addr.tokens.DAI[network],
+			addr.tokens.USDT[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("WrapperSushi is deployed at: ", wrapperSushi.address);
 
-	const lp2lp = await deployWithProxy(
-        LP2LP, 
-        OwnableProxy, 
-        'LP2LP',
-		addr.tokens.vBNT[network]
-    );
-	console.log("LP2LP is deployed at: ", lp2lp.address);
+		const tokenRewards = await deployWithProxy(
+			TokenRewards,
+			OwnableProxy,
+			'TokenRewards'
+		);
+		console.log("TokenRewards is deployed at: ", tokenRewards.address);
+	
+		const plexusOracle = await deployWithProxy(
+			PlexusOracle, 
+			OwnableProxy, 
+			'PlexusOracle',
+			addr.swaps.uniswap[network],
+			addr.tokens.USDC[network]
+		);
+		console.log("PlexusOracle is deployed at: ", plexusOracle.address);
+	
+		const tier2Farm = await deployWithProxy(
+			Tier2Farm, 
+			OwnableProxy, 
+			'Tier2FarmController',
+			addr.AutoStake[network],
+			addr.tokens.FARM[network]
+		);
+		console.log("Tier2Farm is deployed at: ", tier2Farm.address);
+	
+		const tier1Staking = await deployWithProxy(
+			Tier1Staking, 
+			OwnableProxy, 
+			'Tier1FarmController',
+			tier2Farm.address,
+			plexusOracle.address
+		);
+		console.log("Tier1Staking is deployed at: ", tier1Staking.address);
+	
+		const core = await deployWithProxy(
+			Core, 
+			OwnableProxy, 
+			'Core',
+			addr.tokens.WETH[network],
+			wrapper.address
+		);
+		console.log("Core is deployed at: ", core.address);
+	
+		const tier2Aave = await deployWithProxy(
+			Tier2Aave, 
+			OwnableProxy, 
+			'Tier2AaveFarmController',
+			addr.AaveLendingPoolV2[network],
+			addr.tokens.DAI[network],
+			addr.tokens.aDAI[network]
+		);
+		console.log("Tier2Aave is deployed at: ", tier2Aave.address);
+	
+		const tier2Pickle = await deployWithProxy(
+			Tier2Pickle, 
+			OwnableProxy, 
+			'Tier2PickleFarmController',
+			addr.StakingRewards[network],
+			addr.tokens.PICKLE[network]
+		);
+		console.log("Tier2Pickle is deployed at: ", tier2Pickle.address);
+	
+		const lp2lp = await deployWithProxy(
+			LP2LP, 
+			OwnableProxy, 
+			'LP2LP',
+			addr.tokens.vBNT[network]
+		);
+		console.log("LP2LP is deployed at: ", lp2lp.address);
+	
+		const tier2Aggregator = await deployWithProxy(
+			Tier2Aggregator, 
+			OwnableProxy, 
+			'Tier2AggregatorFarmController',
+			addr.tokens.pUNI_V2[network],
+			addr.tokens.UNI_V2[network]
+		);
+		console.log("Tier2Aggregator is deployed at: ", tier2Aggregator.address);
+	
+		const plexusCoin = await (await PlexusCoin.deploy()).deployed();
+		console.log("PlexusCoin is deployed at: ", plexusCoin.address);
 
-	const tier2Aggregator = await deployWithProxy(
-        Tier2Aggregator, 
-        OwnableProxy, 
-        'Tier2AggregatorFarmController',
-		addr.tokens.pUNI_V2[network],
-		addr.tokens.UNI_V2[network]
-    );
-	console.log("Tier2Aggregator is deployed at: ", tier2Aggregator.address);
-
-	const plexusCoin = await (await PlexusCoin.deploy()).deployed();
-	console.log("PlexusCoin is deployed at: ", plexusCoin.address);
-
-	console.log("");
-	// then setup the contracts
-	console.log("Setting up contracts... ");
-	await tokenRewards.updateOracleAddress(plexusOracle.address);
-	await tokenRewards.updateStakingTokenAddress(plexusCoin.address);
- 
-	await plexusOracle.updateRewardAddress(tokenRewards.address);
-	await plexusOracle.updateCoreAddress(core.address);
-	await plexusOracle.updateTier1Address(tier1Staking.address);
-
-	await core.setOracleAddress(plexusOracle.address);
-	await core.setStakingAddress(tier1Staking.address);
-	await core.setConverterAddress(wrapper.address);
-
-	await tier1Staking.updateOracleAddress(plexusOracle.address);
-
-	console.log("");
-	// setup tier 1 staking
-	console.log("Setting up Tier1Staking... ");
-	await tier1Staking.addOrEditTier2ChildStakingContract("FARM", tier2Farm.address);
-	await tier1Staking.addOrEditTier2ChildStakingContract("DAI", tier2Aave.address);
-	await tier1Staking.addOrEditTier2ChildStakingContract("PICKLE", tier2Pickle.address);
-
-	console.log("");
-	// setup ownership of tier2 contracts ownership
-	console.log("Setting up ownerships of Tier2 contracts... ");
-	await tier2Farm.changeOwner(tier1Staking.address);
-	await tier2Aave.changeOwner(tier1Staking.address);
-	await tier2Pickle.changeOwner(tier1Staking.address);
+		// airdrop
+		const airdrop = await deployWithProxy(
+			Airdrop, 
+			OwnableProxy, 
+			'Airdrop',
+			plexusCoin.address,
+			[addr1.address, addrs[0].address]
+		);
+		console.log("Airdrop is deployed at: ", airdrop.address);
+	
+		console.log("");
+		// then setup the contracts
+		console.log("Setting up contracts... ");
+		await tokenRewards.updateOracleAddress(plexusOracle.address);
+		await tokenRewards.updateStakingTokenAddress(plexusCoin.address);
+	 
+		await plexusOracle.updateRewardAddress(tokenRewards.address);
+		await plexusOracle.updateCoreAddress(core.address);
+		await plexusOracle.updateTier1Address(tier1Staking.address);
+	
+		await core.setOracleAddress(plexusOracle.address);
+		await core.setStakingAddress(tier1Staking.address);
+		await core.setConverterAddress(wrapper.address);
+	
+		await tier1Staking.updateOracleAddress(plexusOracle.address);
+	
+		console.log("");
+		// setup tier 1 staking
+		console.log("Setting up Tier1Staking... ");
+		await tier1Staking.addOrEditTier2ChildStakingContract("FARM", tier2Farm.address);
+		await tier1Staking.addOrEditTier2ChildStakingContract("DAI", tier2Aave.address);
+		await tier1Staking.addOrEditTier2ChildStakingContract("PICKLE", tier2Pickle.address);
+	
+		console.log("");
+		// setup ownership of tier2 contracts ownership
+		console.log("Setting up ownerships of Tier2 contracts... ");
+		await tier2Farm.changeOwner(tier1Staking.address);
+		await tier2Aave.changeOwner(tier1Staking.address);
+		await tier2Pickle.changeOwner(tier1Staking.address);
+	}
 
 	console.log("");
 	console.log("Successfully Deployed!");
