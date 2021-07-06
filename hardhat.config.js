d66646c94d931c1b50973b6b5b6360cb6b125c68
Lucifer H <trmlucifer2020@gmail.com>
2021-07-07 02:19:53 +0800

    make sure the contracts are deployed smoothly in binance and polygon

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,60 +1,91 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("dotenv").config();
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-solhint');
-require('solidity-coverage');
-
-module.exports = {
-  defaultNetwork: "hardhat",
-  solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  networks: {
-    hardhat: {
-      gas: "auto",
-      gasPrice: "auto",
-      gasMultiplier: 20,
-      blockGasLimit: 90000000000000,
-      forking: {
-        url: process.env.RPC_NODE_URL,
-        blockNumber: 11997864,
-      }
-    },
-
-    mainnet: {
-      gas: "auto",
-      gasPrice: "auto",
-      gasMultiplier: 20,
-      blockGasLimit: 90000000000000,
-      accounts: {
-        mnemonic: process.env.MNEMONIC
-      },
-      url: process.env.RPC_NODE_URL,
-      chainId: 1
-    },
-    
-    kovan: {
-      gas: "auto",
-      gasPrice: "auto",
-      gasMultiplier: 20,
-      blockGasLimit: 90000000000000,
-      accounts: {
-        mnemonic: process.env.MNEMONIC
-      },
-      url: process.env.RPC_NODE_URL_KOVAN,
-      chainId: 42
-    }
-  },
-
-  mocha: {
-    timeout: 2000000
-  }
-};
+ require("dotenv").config();
+ require('@nomiclabs/hardhat-waffle');
+ require('@nomiclabs/hardhat-solhint');
+ require('solidity-coverage');
+ 
+ module.exports = {
+   defaultNetwork: "hardhat",
+   solidity: {
+     version: "0.8.0",
+     settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+     }
+   },
+   networks: {
+     hardhat: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       forking: {
+         url: process.env.RPC_NODE_URL,
+         blockNumber: 11997864,
+       },
+       timeout: 2000000
+     },
+ 
+     mainnet: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL,
+       chainId: 1,
+       timeout: 2000000
+     },
+     
+     kovan: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL_KOVAN,
+       chainId: 42,
+       timeout: 2000000
+     },
+     
+     binance: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL_BINANCE,
+       chainId: 56,
+       timeout: 20000000
+     },
+     
+     matic: {
+       gas: "auto",
+       gasPrice: "auto",
+       gasMultiplier: 20,
+       blockGasLimit: 90000000000000,
+       accounts: {
+         mnemonic: process.env.MNEMONIC
+       },
+       url: process.env.RPC_NODE_URL_MATIC,
+       chainId: 137,
+       timeout: 20000000
+     },
+ 
+   },
+ 
+   mocha: {
+     timeout: 2000000
+   }
+ };
+ 
