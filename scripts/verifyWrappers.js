ebb41070801d5bc0598bb63fc6c1a2b168f2a91c
terminator0x <mulish.tc@gmail.com>
2021-08-10 22:02:17 +0300

    modified verifaction script to be more detailed

--- scripts/verifyWrappers.js
+++ scripts/verifyWrappers.js
@@ -3,21 +3,44 @@ const hre = require("hardhat");
 
 async function main() {
 	const netinfo = await ethers.provider.getNetwork();
-	let network = netinfo.name;
-	if (network === "unknown")
-		network = "mainnet";
 
-	// Verify Wrapper and Wrapper Proxy
-	const wrapper = addresses.WrapAndUnWrap
-	await verifyContract(wrapper, network);
-	const wrapperProxy = addresses.WrapAndUnWrapProxy
-	await verifyContract(wrapperProxy, network);
-	const wrapperSushi = addresses.WrapAndUnWrapSushi
-	await verifyContract(wrapperSushi, network);
-	const wrapperSushiProxy = addresses.WrapAndUnWrapSushiProxy
-	await verifyContract(wrapperSushiProxy, network);
+	console.log('netinfo', netinfo.name);
+	const network = netinfo.chainId === 1 ? "mainnet" :
+			  netinfo.chainId === 42 ? "kovan" :
+			  netinfo.chainId === 56 ? "binance" :
+			  netinfo.chainId === 137 ? "matic" : 'mainnet';
 
+
+	const chainId = netinfo.chainId;
 	console.log("============================================================");
+	console.log("");
+	console.log("Verification Started ...");
+	console.log("Verifying contracts on " + network + " (chainId: " + chainId + ") ...");
+	console.log("");
+
+
+	if (network === "mainnet" || network === "kovan") {
+
+		// Verify Wrapper and Wrapper Proxy
+		const wrapper = addresses.WrapAndUnWrap
+		await verifyContract(wrapper, network);
+		const wrapperProxy = addresses.WrapAndUnWrapProxy
+		await verifyContract(wrapperProxy, network);
+		const wrapperSushi = addresses.WrapAndUnWrapSushi
+		await verifyContract(wrapperSushi, network);
+		const wrapperSushiProxy = addresses.WrapAndUnWrapSushiProxy
+		await verifyContract(wrapperSushiProxy, network);
+
+
+		console.log("");
+		console.log("Successfully Verified Wrapper(s)!");
+		console.log("============================================================");
+
+	} else {
+		console.log("Sorry verification is not yet enabled for this network yet!");
+	}
+		
+
 }
 
 async function verifyContract(contractInfo, network) {
@@ -25,8 +48,8 @@ async function verifyContract(contractInfo, network) {
 		const contract = contractInfo[network]
 		if (contract) {
 			if (contract && contract.address.length > 0) {
-				console.log('contractAddress', contract.address)
-				console.log('contractArgs', contract.args)
+				console.log('Contract Address', contract.address)
+				console.log('Contract Args', contract.args)
 				try {
 					await hre.run("verify:verify", {
 						address: contract.address,
